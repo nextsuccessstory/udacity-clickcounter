0c30e817dd5a0689c1178d03c3d772474548177c
Glenn-Arc Cachuela Pineda <glennpineda@hotmail.com>
2017-12-08 15:28:25 -0800

    feat: Added click counter label and button.

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        var label = UILabel()
+        label.frame = CGRect(x:150, y:150, width:60, height:60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRect(x:150, y:250, width:60, height:60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
     }
 
 

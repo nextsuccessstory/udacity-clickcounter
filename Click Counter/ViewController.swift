0edc290f1f2004d6f6330677556cc401bfa09f8c
Glenn-Arc Cachuela Pineda <glennpineda@hotmail.com>
2017-12-08 16:10:17 -0800

    feat: Added all the required code lines for the subject.

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count:Int = 0
+    var label:UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var label = UILabel()
@@ -17,15 +22,39 @@ class ViewController: UIViewController {
         label.text = "0"
         
         self.view.addSubview(label)
+        self.label = label
         
         //Button
         var button = UIButton()
-        button.frame = CGRect(x:150, y:250, width:60, height:60)
-        button.setTitle("Click", for: .normal)
+        button.frame = CGRect(x:150, y:250, width:100, height:60)
+        button.setTitle("Increase", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(incrementCount), for: UIControlEvents.touchUpInside)
+        
+        var button2 = UIButton()
+        button2.frame = CGRect(x:150, y:300, width:100, height:60)
+        button2.setTitle("Decrease", for: .normal)
+        button2.setTitleColor(UIColor.red, for: .normal)
+        self.view.addSubview(button2)
+        
+        button2.addTarget(self, action: #selector(decrementCount), for: UIControlEvents.touchUpInside)
+    }
+    
+    @objc func incrementCount() {
+        self.count+=1
+        self.label.text = "\(self.count)"
+        
+        self.view.backgroundColor = UIColor.lightGray
     }
 
+    @objc func decrementCount() {
+        self.count-=1
+        self.label.text = "\(self.count)"
+        
+        self.view.backgroundColor = UIColor.darkGray
+    }
 
 }
 
